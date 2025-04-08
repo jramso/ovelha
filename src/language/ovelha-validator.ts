7753cfbcfa72accb52a7f907e8f20f299d71258c
jramso <josue.rsou2@gmail.com>
2025-04-08 12:14:48 -0300

    feat(add): test.ov fix: removendo Persons e Greetings do generator e Validator

--- src/language/ovelha-validator.ts
+++ src/language/ovelha-validator.ts
@@ -1,5 +1,5 @@
-import type { ValidationAcceptor, ValidationChecks } from 'langium';
-import type { OvelhaAstType, Person } from './generated/ast.js';
+import type { ValidationChecks } from 'langium';
+import type { OvelhaAstType } from './generated/ast.js';
 import type { OvelhaServices } from './ovelha-module.js';
 
 /**
@@ -9,7 +9,7 @@ export function registerValidationChecks(services: OvelhaServices) {
     const registry = services.validation.ValidationRegistry;
     const validator = services.validation.OvelhaValidator;
     const checks: ValidationChecks<OvelhaAstType> = {
-        Person: validator.checkPersonStartsWithCapital
+        
     };
     registry.register(checks, validator);
 }
@@ -19,13 +19,5 @@ export function registerValidationChecks(services: OvelhaServices) {
  */
 export class OvelhaValidator {
 
-    checkPersonStartsWithCapital(person: Person, accept: ValidationAcceptor): void {
-        if (person.name) {
-            const firstChar = person.name.substring(0, 1);
-            if (firstChar.toUpperCase() !== firstChar) {
-                accept('warning', 'Person name should start with a capital.', { node: person, property: 'name' });
-            }
-        }
-    }
 
 }
